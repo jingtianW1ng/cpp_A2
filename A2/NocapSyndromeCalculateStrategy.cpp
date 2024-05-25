5c7b8867e81e5f7b06f6a2b0d3911e24f8e6201c
jingtianW1ng <ajssjaus@gmail.com>
2024-05-26 04:16:09 +0930

    Update NocapSyndromeCalculateStrategy class calculateAlertLevel method

--- A2/NocapSyndromeCalculateStrategy.cpp
+++ A2/NocapSyndromeCalculateStrategy.cpp
@@ -3,8 +3,21 @@
 #include "Patient.h"
 NocapSyndromeCalculateStrategy::NocapSyndromeCalculateStrategy()
 {
+    alertLevel = AlertLevel::Green;
 }
 
 void NocapSyndromeCalculateStrategy::calculateAlertLevel(const Vitals* v, Patient* p)
 {
+    if (v->SPO2() > 92 && v->SPO2() < 94)
+    {
+        alertLevel = AlertLevel::Yellow;
+    }
+    if (v->SPO2() > 90 && v->SPO2() < 92)
+    {
+        alertLevel = AlertLevel::Orange;
+    }
+    if (v->SPO2() < 90)
+    {
+        alertLevel = AlertLevel::Red;
+    }
 }
