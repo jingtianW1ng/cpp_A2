d65da1a76aac1a0cb5c0e4cbf9ea2724b123c6ed
jingtianW1ng <ajssjaus@gmail.com>
2024-05-26 03:43:18 +0930

    Update calculateAlertLevel method on AmogusSusCalculateStrategy class

--- A2/AmogusSusCalculateStrategy.cpp
+++ A2/AmogusSusCalculateStrategy.cpp
@@ -8,4 +8,24 @@ AmogusSusCalculateStrategy::AmogusSusCalculateStrategy()
 
 void AmogusSusCalculateStrategy::calculateAlertLevel(const Vitals* v, Patient* p)
 {
+    if (v->HR() < 200)
+    {
+        alertLevel = AlertLevel::Green;
+        p->setAlertLevel(alertLevel);
+    }
+    else if (200 < v->HR() < 200)
+    {
+        alertLevel = AlertLevel::Yellow;
+        p->setAlertLevel(alertLevel);
+    }
+    else if (210 < v->HR() < 220)
+    {
+        alertLevel = AlertLevel::Orange;
+        p->setAlertLevel(alertLevel);
+    }
+    else
+    {
+        alertLevel = AlertLevel::Red;
+        p->setAlertLevel(alertLevel);
+    }
 }
