c5256df3cb9c49659176c23a38b9f5313ecd0fce
jingtianW1ng <ajssjaus@gmail.com>
2024-05-26 04:16:42 +0930

    Update TicctoccBrainDamageCalculateStrategy class calculateAlertLevel method

--- A2/TicctoccBrainDamageCalculateStrategy.cpp
+++ A2/TicctoccBrainDamageCalculateStrategy.cpp
@@ -3,8 +3,17 @@
 #include "Patient.h"
 TicctoccBrainDamageCalculateStrategy::TicctoccBrainDamageCalculateStrategy()
 {
+    alertLevel = AlertLevel::Green;
 }
 
 void TicctoccBrainDamageCalculateStrategy::calculateAlertLevel(const Vitals* v, Patient* p)
 {
+    if (p->age() < 35 && v->BA() < 10)
+    {
+        alertLevel = AlertLevel::Red;
+    }
+    if (p->age() >= 35 && v->BA() < 20)
+    {
+        alertLevel = AlertLevel::Red;
+    }
 }
