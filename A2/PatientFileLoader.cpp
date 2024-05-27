d67ae4988d3dccd4fd2f470bc47cee44a7e58bd9
jingtianW1ng <ajssjaus@gmail.com>
2024-05-27 13:29:27 +0930

    Update reading string line logic

--- A2/PatientFileLoader.cpp
+++ A2/PatientFileLoader.cpp
@@ -21,6 +21,29 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile()
     std::ifstream inFile(_file);
     if (inFile.is_open()) {
         // TODO: load your file here
+        //using while loop looping each line
+        while (!inFile.eof())
+        {
+            //read every line
+            std::string line;
+            std::getline(inFile, line);
+            //make line to istringstream
+            std::istringstream issLine(line);
+            std::string destString;
+            std::vector<std::string> stringList;
+
+            //add every string between '/' to a list
+            while (std::getline(issLine, destString, '/')) {
+                stringList.push_back(destString);
+            }
+
+            //start add everythong into Patient
+            for (const auto& s : stringList) {
+                std::cout << "name: " << s << std::endl;
+            }
+        }
+        //close file when reach the end
+        inFile.close();
     }
 
     return patients;
