2364daf5e65904bdf79504171e6ee85d26db92ae
jingtianW1ng <ajssjaus@gmail.com>
2024-05-28 08:32:52 +0930

    Fix NocapSyndromeCalculateStrategy strategy logic

--- A2/NocapSyndromeCalculateStrategy.cpp
+++ A2/NocapSyndromeCalculateStrategy.cpp
@@ -8,11 +8,11 @@ NocapSyndromeCalculateStrategy::NocapSyndromeCalculateStrategy()
 
 void NocapSyndromeCalculateStrategy::calculateAlertLevel(const Vitals* v, Patient* p)
 {
-    if (v->SPO2() > 92 && v->SPO2() < 94)
+    if (v->SPO2() >= 92 && v->SPO2() < 94)
     {
         alertLevel = AlertLevel::Yellow;
     }
-    if (v->SPO2() > 90 && v->SPO2() < 92)
+    if (v->SPO2() >= 90 && v->SPO2() < 92)
     {
         alertLevel = AlertLevel::Orange;
     }
